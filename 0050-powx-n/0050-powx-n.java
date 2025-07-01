51b588194617010f2ad02e9d16743ac2ff3ae001
Joice Gloriya <joicegloriyaa@gmail.com>
2025-07-01 18:24:02 +0530

    Time: 0 ms (100%), Space: 41.8 MB (88.42%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,5 +1,6 @@
 class Solution {
     public double myPow(double x, int n) {
+        // TC -> O(log N)
         if (x == 1 || x == 0)
             return x;
         double ans = 1.0;
@@ -8,15 +9,13 @@ class Solution {
             nn = -1 * nn;
         while (nn > 0) {
             if (nn % 2 == 1) {
-                ans = ans * x;
-                nn = nn - 1;
-            } else {
-                x = x * x;
-                nn = nn / 2;
-            }
+                ans = ans * x;           
+            } 
+            x = x * x;
+            nn = nn / 2;  
         }
         if (n < 0)
-            ans = (double) (1.0) / (double) (ans);
+            ans = (double) (1.0) / (ans);
         return ans;
     }
 }
