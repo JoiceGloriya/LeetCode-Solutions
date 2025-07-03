7f7053b9f46d8296fe763b5df658c1bc512e5218
Joice Gloriya <joicegloriyaa@gmail.com>
2025-07-03 10:06:20 +0530

    Time: 373 ms (27.79%), Space: 41 MB (71.63%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -7,10 +7,10 @@ class Solution {
         for(int i = 0; i < board.length; i ++) {
             for(int j = 0; j < board[0].length; j ++) {
                 if(board[i][j] == '.') {
-                    for(int value = 1; value <= 9; value++) {
-                        char c = (char)(value + '0');
-                        if(isSafe(board, i, j, c)) {
-                            board[i][j] = c;
+                    for(char value = '1'; value <= '9'; value++) {
+                        // char c = (char)(value + '0');
+                        if(isSafe(board, i, j, value)) {
+                            board[i][j] = value;
                             if(backtrack(board)) return true;
                             else board[i][j] = '.';
                         } 
@@ -23,7 +23,7 @@ class Solution {
     }
 
     boolean isSafe(char[][] board, int row, int col, char value) {
-        for(int i = 0; i <= 8; i ++) {
+        for(int i = 0; i < 9; i ++) {
             if(board[row][i] == value) return false;
             if(board[i][col] == value) return false;
             if(board[3 * (row / 3) + (i / 3)][3 * (col / 3) + (i % 3)] == value) return false;
