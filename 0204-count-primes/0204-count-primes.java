49ad1f716a88ff21f2cb87b60b957c5f2fe1fa44
Joice Gloriya <joicegloriyaa@gmail.com>
2025-07-04 19:16:57 +0530

    Time: 112 ms (26.05%), Space: 65.1 MB (6.01%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,7 +1,10 @@
 class Solution {
     public int countPrimes(int n) {
+        // SC -> O(N + 1)
         int[] prime = new int[n + 1];
-        for(int i = 2; i <= n; i ++) prime[i] = 1;
+        // TC -> O(N)
+        Arrays.fill(prime, 1);
+        // Sieve Of Eratosthenes: TC -> O(Nlog(logN))
         for(int i = 2; i * i <= n; i ++) {
             if(prime[i] == 1) {
                 for(int j = i * i; j <= n; j += i)
@@ -9,6 +12,7 @@ class Solution {
             }
         }
         int ct = 0;
+        // TC -> O(N)
         for(int i = 2; i < n; i ++)
             if(prime[i] == 1) ct++;
         return ct;
