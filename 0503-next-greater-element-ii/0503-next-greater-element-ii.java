521fa389f0227423a1ac9b6f90a965f4bc07d4aa
Joice Gloriya <joicegloriyaa@gmail.com>
2025-07-08 06:57:09 +0530

    Time: 15 ms (70.41%), Space: 46.9 MB (6.64%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -4,12 +4,13 @@ class Solution {
         int n = nums.length;
         int[] ans = new int[n];
         Stack<Integer> stk = new Stack<>();
-        for(int i = 2 * n - 1; i >= 0; i --) {// => O(2N)
-            while(!stk.isEmpty() && nums[i % n] >= stk.peek())  // => O(2N)
+        for (int i = 2 * n - 1; i >= 0; i--) {// => O(2N)
+            int ind = i % n;
+            while (!stk.isEmpty() && nums[ind] >= stk.peek()) // => O(2N) [Wrst case -> arr is desc]
                 stk.pop();
-           if(i < n)
+            if (i < n)
                 ans[i] = stk.isEmpty() ? -1 : stk.peek();
-            stk.push(nums[i % n]);
+            stk.push(nums[ind]);
         }
         return ans;
     }
