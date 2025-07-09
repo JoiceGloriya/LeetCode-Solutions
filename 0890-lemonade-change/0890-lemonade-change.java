cbf173947cc392ed2336247078348e69645dbcde
Joice Gloriya <joicegloriyaa@gmail.com>
2025-07-09 17:25:36 +0530

    Time: 2 ms (93.35%), Space: 55.7 MB (85.71%) - LeetHub

--- 0890-lemonade-change/0890-lemonade-change.java
+++ 0890-lemonade-change/0890-lemonade-change.java
@@ -1,25 +1,26 @@
 class Solution {
     public boolean lemonadeChange(int[] bills) {
-        if(bills[0] != 5) return false;
+        if (bills[0] != 5)
+            return false;
         int no_of_fives = 1, no_of_tens = 0, no_of_twenties = 0;
-        for(int i = 1; i < bills.length; i ++) {
-            if(bills[i] == 5)
+        for (int i = 1; i < bills.length; i++) {
+            if (bills[i] == 5)
                 no_of_fives++;
-            else if(bills[i] == 10) {
+            else if (bills[i] == 10) {
                 no_of_tens++;
-                if(no_of_fives > 0) no_of_fives --;
-                else return false;
-            }
-            else {
+                if (no_of_fives > 0)
+                    no_of_fives--;
+                else
+                    return false;
+            } else {
                 no_of_twenties++;
-                if(no_of_fives >= 3 || (no_of_fives >= 1 && no_of_tens >= 1)) {
-                    if(no_of_fives >= 1 && no_of_tens >= 1) {
-                        no_of_fives--;
-                        no_of_tens--;
-                    }
-                    else no_of_fives -= 3;
-                }
-                else return false;
+                if (no_of_fives >= 1 && no_of_tens >= 1) {
+                    no_of_fives--;
+                    no_of_tens--;
+                } else if (no_of_fives >= 3)
+                    no_of_fives -= 3;
+                else
+                    return false;
             }
         }
         return true;
