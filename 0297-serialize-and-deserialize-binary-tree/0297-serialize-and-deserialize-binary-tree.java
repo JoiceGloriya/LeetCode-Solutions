f97e0828e317be476adc011c7bea382a95a551a5
Joice Gloriya <joicegloriyaa@gmail.com>
2025-07-12 19:00:17 +0530

    Time: 17 ms (41.88%), Space: 46 MB (56.02%) - LeetHub

--- 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.java
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.java
@@ -13,18 +13,32 @@ public class Codec {
     public String serialize(TreeNode root) {
         if (root == null)
             return "";
+
         StringBuilder sb = new StringBuilder();
         Queue<TreeNode> queue = new LinkedList<>();
         queue.offer(root);
-        while (!queue.isEmpty()) {
-            TreeNode node = queue.poll();
-            if (node == null) {
-                sb.append("# ");
+        sb.append(root.val + " ");
 
-            } else {
-                sb.append(node.val + " ");
-                queue.offer(node.left);
-                queue.offer(node.right);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+
+            for (int i = 0; i < size; i++) {
+            TreeNode node = queue.poll();
+
+                if (node.left == null) {
+                    sb.append("# ");
+                } else {
+                    sb.append(node.left.val + " ");
+                    queue.offer(node.left);  
+                }
+
+                if (node.right == null) {
+                    sb.append("# ");
+                } else {
+                    sb.append(node.right.val + " ");
+                    queue.offer(node.right);  
+                }
+
             }
         }
         return sb.toString();
@@ -34,19 +48,23 @@ public class Codec {
     public TreeNode deserialize(String data) {
         if (data == "")
             return null;
+
         String[] values = data.split(" ");
         TreeNode root = new TreeNode(Integer.parseInt(values[0]));
         Queue<TreeNode> queue = new LinkedList<>();
         queue.offer(root);
+
         for (int i = 1; i < values.length; i++) {
             TreeNode node = queue.poll();
+
             if (!values[i].equals("#")) {
                 TreeNode left = new TreeNode(Integer.parseInt(values[i]));
                 node.left = left;
                 queue.offer(left);
             }
-            if (!values[++i].equals("#")) {
-                TreeNode right = new TreeNode(Integer.parseInt(values[i]));//i has already been incremented
+            i++;
+            if (!values[i].equals("#")) {
+                TreeNode right = new TreeNode(Integer.parseInt(values[i]));    //i has already been incremented
                 node.right = right;
                 queue.offer(right);
             }
