88c291487fcb60656bbacb24c39fc511194ed778
Joice Gloriya <joicegloriyaa@gmail.com>
2025-07-13 16:24:59 +0530

    Time: 2 ms (96.05%), Space: 45.5 MB (28.74%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
@@ -16,56 +16,62 @@
 class Solution {
     Stack<TreeNode> leftStack = new Stack<>();
     Stack<TreeNode> rightStack = new Stack<>();
-    int next = 0, prev = 0;
+    // int next = 0, prev = 0;
 
     void buildLeft(TreeNode root) {
         TreeNode cur = root;
-        while(cur != null) {
+        while (cur != null) {
             leftStack.push(cur);
             cur = cur.left;
         }
     }
-    void buildRight(TreeNode root) {
+
+    void buildRight(TreeNode root) { //reverse inorder
         TreeNode cur = root;
-        while(cur != null) {
+        while (cur != null) {
             rightStack.push(cur);
             cur = cur.right;
         }
     }
+
     int next() {
         TreeNode node = leftStack.pop();
         int value = node.val;
-        if(node.right != null) {
+        if (node.right != null) {
             TreeNode temp = node.right;
-            while(temp != null) {
+            while (temp != null) {
                 leftStack.push(temp);
                 temp = temp.left;
             }
         }
         return value;
     }
+
     int prev() {
         TreeNode node = rightStack.pop();
         int value = node.val;
-        if(node.left != null) {
+        if (node.left != null) {
             TreeNode temp = node.left;
-            while(temp != null) {
+            while (temp != null) {
                 rightStack.push(temp);
                 temp = temp.right;
             }
         }
         return value;
     }
+
     public boolean findTarget(TreeNode root, int k) {
         buildLeft(root);
         buildRight(root);
         int left = next();
         int right = prev();
-        while(left < right) {
-        if(left + right == k) return true;
-        else if(left + right < k)
-            left = next();
-        else right = prev();            
+        while (left < right) {
+            if (left + right == k)
+                return true;
+            else if (left + right < k)
+                left = next();
+            else
+                right = prev();
         }
         return false;
     }
