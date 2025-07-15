d3cd4b2a4a81670543a81e74cd7168e2f8873522
Joice Gloriya <joicegloriyaa@gmail.com>
2025-07-15 23:30:56 +0530

    Time: 6 ms (73.55%), Space: 46.1 MB (16.98%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,44 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+
+        List<List<Integer>> adj = new ArrayList<>();
+
+        for (int i = 0; i < numCourses; i++) {
+            adj.add(new ArrayList<>());
+        }
+        
+        for (int edge[] : prerequisites) {
+            int u = edge[0];
+            int v = edge[1];
+            adj.get(u).add(v);
+        }
+
+        int[] indegree = new int[numCourses];
+        for (int i = 0; i < numCourses; i++) {
+            for (int j : adj.get(i)) {
+                indegree[j]++;
+            }
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i =0; i < numCourses; i ++) {
+            if(indegree[i] == 0) {
+                queue.offer(i);
+            }
+        }
+
+        int ct = 0;
+        while(!queue.isEmpty()) {
+            ct++;
+            int num = queue.poll();
+            for (int i : adj.get(num)) {
+                indegree[i]--;
+                if(indegree[i] == 0) {
+                    queue.offer(i);
+                }
+            }
+        }
+
+        return numCourses == ct;
+    }
+}
