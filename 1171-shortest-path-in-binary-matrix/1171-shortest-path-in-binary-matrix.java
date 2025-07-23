ddaa5eadb56da8b986ca39091a8ead6e598ce403
Joice Gloriya <joicegloriyaa@gmail.com>
2025-07-23 05:37:34 +0530

    Time: 14 ms (78.83%), Space: 45.7 MB (42.16%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.java
@@ -1,35 +1,25 @@
 class Solution {
     class Pair {
-        int row, col;
+        int row, col, steps;
 
-        public Pair(int row, int col) {
+        public Pair(int row, int col, int steps) {
             this.row = row;
             this.col = col;
+            this.steps = steps;
         }
     }
 
     public int shortestPathBinaryMatrix(int[][] grid) {
         
-        List<List<Integer>> adj = new ArrayList<>();
         int n = grid.length;
         boolean[][] visited = new boolean[n][n];
         Queue<Pair> queue = new LinkedList<>();
 
         if (grid[0][0] != 0 || grid[n - 1][n - 1] != 0)
             return -1;
-
-        for (int i = 0; i < n; i++) {
-            List<Integer> row = new ArrayList<>();
-            for (int j = 0; j < n; j++) {
-                row.add(grid[i][j]);
-            }
-            adj.add(row);
-        }
-
         
-        queue.offer(new Pair(0, 0));
+        queue.offer(new Pair(0, 0, 1));
         visited[0][0] = true;
-        int ct = 1;
 
         while (!queue.isEmpty()) {
             int size = queue.size();
@@ -37,10 +27,11 @@ class Solution {
             for (int i = 0; i < size; i++) {
                 int r = queue.peek().row;
                 int c = queue.peek().col;
+                int steps = queue.peek().steps;
                 queue.poll();
                 
                 if (r == n - 1 && c == n - 1)
-                    return ct;
+                    return steps;
 
                 for (int dRow = -1; dRow <= 1; dRow++) {
                     for (int dCol = -1; dCol <= 1; dCol++) {
@@ -50,12 +41,12 @@ class Solution {
                         if (nRow >= 0 && nRow < n && nCol >= 0 && nCol < n
                                 && grid[nRow][nCol] == 0 && !visited[nRow][nCol]) {
                             visited[nRow][nCol] = true;
-                            queue.offer(new Pair(nRow, nCol));
+                            queue.offer(new Pair(nRow, nCol, steps+ 1));
                         }
                     }
                 }
             }
-            ct++;
+          
         }
 
         return -1;
