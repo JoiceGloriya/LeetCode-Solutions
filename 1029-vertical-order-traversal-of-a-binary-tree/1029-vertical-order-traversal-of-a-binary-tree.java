822d824c0e4c2a15c73920003aaabb0a6ffc6c96
Joice Gloriya <joicegloriyaa@gmail.com>
2025-07-25 14:10:10 +0530

    Time: 4 ms (42.68%), Space: 42.6 MB (51.13%) - LeetHub

--- 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.java
+++ 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.java
@@ -13,8 +13,6 @@
  *     }
  * }
  */
-class Solution {
-
     class Triplet {
         TreeNode node = new TreeNode();
         int vertical, level;
@@ -25,39 +23,38 @@ class Solution {
             this.level = level;
         }
     }
+class Solution {
+
 
     public List<List<Integer>> verticalTraversal(TreeNode root) {
-        List<List<Integer>> list = new ArrayList<>();
         TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> map = new TreeMap<>();
         Queue<Triplet> queue = new LinkedList<>();
         queue.offer(new Triplet(root, 0, 0));
+        
         while(!queue.isEmpty()) {
             Triplet triplet = queue.poll();
             TreeNode node = triplet.node;
             int x = triplet.vertical;
             int y = triplet.level;
-            // if(!map.containsKey(x)) 
-            //     map.put(x, new TreeMap<>()); 
+
             map.putIfAbsent(x, new TreeMap<>());
-            // if(!map.get(x).containsKey(y))
-            //     map.get(x).put(y, new PriorityQueue<>());
-            map.get(x).putIfAbsent(y, new PriorityQueue<>());
+            map.get(x).putIfAbsent(y, new PriorityQueue<Integer>() );
             map.get(x).get(y).offer(node.val);
 
             if(node.left != null)
                 queue.offer(new Triplet(node.left, x - 1, y + 1));
             if(node.right != null)
-                queue.offer(new Triplet(node.right, x + 1, y + 1));
+                queue.offer(new Triplet(node.right, x + 1, y + 1));    
         }
 
-        for(TreeMap<Integer, PriorityQueue<Integer>> mpp: map.values()) {
-            List<Integer> arr = new ArrayList<>();
+        List<List<Integer>> list = new ArrayList<>();
+        for(TreeMap<Integer, PriorityQueue<Integer>> mpp : map.values()) {
+            List<Integer> ans = new ArrayList<>();
             for(PriorityQueue<Integer> pq: mpp.values()) {
-                 while (!pq.isEmpty()) {
-                    arr.add(pq.poll());
-                }
+                while(!pq.isEmpty())
+                    ans.add(pq.poll());
             }
-    list.add(arr);
+           list.add(ans); 
         }
         return list;
     }
