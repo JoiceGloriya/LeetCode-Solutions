12880e5c5e0139e0ee4521a79ea40cbe4030a37b
Joice Gloriya <joicegloriyaa@gmail.com>
2025-07-29 11:05:36 +0530

    Time: 5 ms (99.18%), Space: 46.4 MB (6.16%) - LeetHub

---
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -0,0 +1,21 @@
+class Solution {
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        // if (matrix == null || matrix.length < 1 || matrix[0].length < 1) {
+        //     return false;
+        // }
+//TC -> O(M + N)
+        int row = 0; // first row
+        int col = matrix[0].length - 1; // last column
+        while (col >= 0 && row < matrix.length) {
+            if (matrix[row][col] == target) {
+                return true;
+            } else if (matrix[row][col] > target) {
+                col--;
+            } else {
+                row++;
+            }
+        }
+
+        return false;
+    }
+}
