f5f5ac40044af1f79216fccf46545f02e7fbe64e
Joice Gloriya <joicegloriyaa@gmail.com>
2025-08-22 12:51:38 +0530

    Time: 118 ms (19.38%), Space: 45.4 MB (81.81%) - LeetHub

--- 0282-expression-add-operators/0282-expression-add-operators.java
+++ 0282-expression-add-operators/0282-expression-add-operators.java
@@ -5,22 +5,24 @@ class Solution {
         return list;
     }
 
-    void backtrack(int startIndex, String num, long sum, long prevNum, int target, String ans, List<String> list) {
+    void backtrack(int startIndex, String num, long currSum, long prevNum, int target, String ans, List<String> list) {
         if (startIndex == num.length()) {
-            if (sum == target) 
-                list.add(new String(ans));
+            if (currSum == target)
+                list.add(ans);
             return;
         }
         for (int i = startIndex; i < num.length(); i++) {
-            if (i > startIndex && num.charAt(startIndex) == '0')
+            if (i > startIndex && num.charAt(startIndex) == '0') // 54 + 0+ 3 -> is valid , but 54 + 03 is invalid
                 break;
+
             long currNum = Long.parseLong(num.substring(startIndex, i + 1));
             if (startIndex == 0) {
                 backtrack(i + 1, num, currNum, currNum, target, ans + currNum, list);
             } else {
-                backtrack(i + 1, num, sum + currNum, currNum, target, (ans + "+" + currNum), list);
-                backtrack(i + 1, num, sum - currNum, -currNum, target, (ans + "-" + currNum), list);
-                backtrack(i + 1, num, ((sum - prevNum) + (prevNum * currNum)), (prevNum * currNum),  target, (ans + "*" + currNum), list);
+                backtrack(i + 1, num, currSum + currNum, currNum, target, (ans + "+" + currNum), list);
+                backtrack(i + 1, num, currSum - currNum, -currNum, target, (ans + "-" + currNum), list);
+                backtrack(i + 1, num, ((currSum - prevNum) + (prevNum * currNum)), (prevNum * currNum), target,
+                        (ans + "*" + currNum), list);
             }
         }
     }
