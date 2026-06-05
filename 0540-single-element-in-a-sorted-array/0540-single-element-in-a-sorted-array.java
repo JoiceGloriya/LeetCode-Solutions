ff22dc3d9c91d72b68495a060d1374d91b115d9e
Joice Gloriya <joicegloriyaa@gmail.com>
2026-06-05 19:11:01 +0530

    Time: 0 ms (100%), Space: 53.1 MB (19.49%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,17 +1,22 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int start = 0;
-        int end = nums.length - 1;
+        int n = nums.length;
+        if(n == 1) return nums[0];
+        if(nums[0] != nums[1]) return nums[0];
+        if(nums[n - 1] != nums[n - 2]) return nums[n - 1];
+        
+        int start = 1;
+        int end = nums.length - 2;
 
         while (start <= end) {
             int mid = start + (end - start) / 2;
-            if((mid - 1 >= 0) && (nums[mid] != nums[mid - 1]) && (mid + 1 <= end  ) && nums[mid] != nums[mid + 1]) return nums[mid];
-            if ((mid % 2 == 0 && (mid + 1 <= end) && nums[mid] == nums[mid + 1]) || (mid % 2 == 1 && (mid - 1 >= 0 ) &&nums[mid] == nums[mid - 1])) {
+            if(nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]) return nums[mid];
+            if ((mid % 2 == 0 && nums[mid] == nums[mid + 1]) || (mid % 2 == 1 && nums[mid] == nums[mid - 1])) {
                 start = mid + 1;
             } else {
                 end = mid - 1;
             }
         }
-        return nums[start];
+        return -1;
     }
 }
