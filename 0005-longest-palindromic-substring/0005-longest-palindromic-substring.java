819686aabcf8397c83598077fdf0aeb1b2ebaef2
Joice Gloriya <joicegloriyaa@gmail.com>
2026-06-18 23:20:10 +0530

    Time: 18 ms (48.23%), Space: 45.3 MB (45.41%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,29 +1,27 @@
 class Solution {
-    boolean isPalindrome(String s, int start, int e) {
-        while (start < e) {
-            if (s.charAt(start) != s.charAt(e))
-                return false;
-            start++;
-            e--;
+
+    String findPalindrome(String s, int start, int end) {
+        while (start >= 0 && end < s.length() && s.charAt(start) == s.charAt(end)) {
+            start--;
+            end++;
         }
-        return true;
+        return s.substring(start + 1, end);
     }
 
     public String longestPalindrome(String s) {
-        int n = s.length();
-        int maxLen = 0;
-        String ans = s.charAt(0) + "";
-
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = i; j < n; j++) {
-                if (isPalindrome(s, i, j)) {
-                    if (maxLen < (j - i + 1)) {
-                        maxLen = j - i + 1;
-                        ans = s.substring(i, j + 1);
-                    }
-                }
-            }
+        if (s.length() == 1) {
+            return s;
         }
-        return ans;
+        int n = s.length();
+        String finalPalindrome = s.substring(0, 1);
+        for (int i = 0; i < n - 1; i++) {
+            String oddCenteredPalindrome = findPalindrome(s, i, i);
+            String evenCenteredPalindrome = findPalindrome(s, i, i + 1);
+            if (oddCenteredPalindrome.length() > finalPalindrome.length())
+                finalPalindrome = oddCenteredPalindrome;
+            if (evenCenteredPalindrome.length() > finalPalindrome.length())
+                finalPalindrome = evenCenteredPalindrome;
+        }
+        return finalPalindrome;
     }
 }
