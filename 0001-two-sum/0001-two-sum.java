d5feda93dc5c992bf38a2cb37443f673fc6cb64d
Joice Gloriya <joicegloriyaa@gmail.com>
2026-06-22 20:42:44 +0530

    Time: 36 ms (39.34%), Space: 44.6 MB (99.95%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,28 +1,20 @@
 class Solution {
     public static int[] twoSum(int[] nums, int target) {
-        //BF : TC -> O(N*N)  SC -> O(1)
-        // int[] arr = new int[2];
-        // for (int i = 0; i < nums.length - 1; i++) {
-        //     for (int j = i + 1; j < nums.length; j++) {
-        //         if (nums[i] + nums[j] == target) {
-        //             arr[0] = i;
-        //             arr[1] = j;
-        //             break;
-        //         }
-        //     }
-        // }
-        // return arr;
-
-        //Optimal (hashing)
-        Map<Integer, Integer> mpp = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            int rem = target - nums[i];
-            if (mpp.containsKey(rem)) {
-                int ind = mpp.get(rem);
-                return new int[] { ind, i };
+        int[] arr = new int[2];
+        for(int i=0; i<nums.length; i++) {
+            for(int j=i+1; j<nums.length; j++) {
+            if(nums[i] + nums[j] == target ) {
+                arr[0] = i;
+                arr[1] = j;
+                break;
             }
-            mpp.put(nums[i], i);
         }
-        return new int[] {};
     }
+    return arr;
+}
+public static void main(String args[]) {
+    int[] nums = {2,7,11,15};
+    int target = 9;
+    int[] arr = twoSum(nums, target);
+}
 }
