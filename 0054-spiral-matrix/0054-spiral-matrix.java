7a2b9151fcbebbbb699dc48d5635e9f1637b8dd9
Joice Gloriya <joicegloriyaa@gmail.com>
2026-06-25 23:25:50 +0530

    Time: 0 ms (100%), Space: 42.9 MB (78.52%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -29,10 +29,4 @@ class Solution {
         }
         return  res;
     }
-    public static void main(String[] args) {
-//        int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-        int[][] matrix = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
-                List<Integer> res = spiralOrder(matrix);
-        System.out.println(res);
-    }
 }
