2b11ac4faa96e7be4401fbbe26d6d7d202c1a844
Joice Gloriya <joicegloriyaa@gmail.com>
2026-06-29 23:14:25 +0530

    Time: 0 ms (100%), Space: 42.5 MB (99.94%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,23 +1,24 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
-        // Time Complexity: O(4^n * n)
+        // Time Complexity: O(4^n * n) at max each index has 4 psblities
         // Space Complexity: O(4^n * n)
-         
+
         List<String> list = new ArrayList<>();
-        if(digits.isEmpty()) return list;
+        if (digits.isEmpty())
+            return list;
         StringBuilder a = new StringBuilder();
-        String[] keys = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        String[] keys = { "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
         backtrack(0, digits, a, list, keys);
         return list;
     }
 
     void backtrack(int startIndex, String digits, StringBuilder a, List<String> list, String[] keys) {
-        if(startIndex == digits.length()) {
+        if (startIndex == digits.length()) {
             list.add(a.toString());
             return;
         }
         String str = keys[digits.charAt(startIndex) - '0'];
-        for(int i = 0; i < str.length(); i ++) {
+        for (int i = 0; i < str.length(); i++) {    //traverse the string
             a.append(str.charAt(i));
             backtrack(startIndex + 1, digits, a, list, keys);
             a.deleteCharAt(a.length() - 1);
