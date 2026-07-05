eb6fdc1959b05cd72dce08c66ac54ff2e2b9dfe5
Joice Gloriya <joicegloriyaa@gmail.com>
2026-07-05 22:09:41 +0530

    Time: 0 ms (100%), Space: 42.4 MB (100%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,38 +1,24 @@
 class Solution {
-    double findPower(double x, long n) {
-        if (n == 0)
-            return 1; //anything power 0 is 1
-
-        double half = findPower(x, n / 2);
-        if (n % 2 == 0)
-            return half * half;
-        else
-            return half * half * x;
-
-    }
-
     public double myPow(double x, int n) {
-        // TC -> O(log N)
-        // if (x == 1 || x == 0)
-        //     return x;
-        // double ans = 1.0;
-        // long nn = n;
-        // if (nn < 0)
-        //     nn = -1 * nn;
-        // while (nn > 0) {
-        //     if (nn % 2 == 1) {
-        //         ans = ans * x;           
-        //     } 
-        //     x = x * x;
-        //     nn = nn / 2;  
-        // }
-        // if (n < 0)
-        //     ans = (double) (1.0) / (ans);
-        // return ans;
-        long nn = (long) n;
+        if (x == 1)
+            return 1;
+        if (x == 0)
+            return 0;
+        double ans = 1.0;
+        long nn = n;
+        if (nn < 0)
+            nn = -1 * nn;
+        while (nn > 0) {
+            if (nn % 2 == 1) {
+                ans = ans * x;
+                nn = nn - 1;
+            } else {
+                x = x * x;
+                nn = nn / 2;
+            }
+        }
         if (n < 0)
-            return 1 / findPower(x, -1 * nn);
-        return findPower(x, nn);
-
+            ans = (double) (1.0) / (double) (ans);
+        return ans;
     }
 }
