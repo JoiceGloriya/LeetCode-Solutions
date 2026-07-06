1eca7b5e65a26961cc5619ef29955f129b94569f
Joice Gloriya <joicegloriyaa@gmail.com>
2026-07-06 11:06:59 +0530

    Time: 6 ms (93.21%), Space: 46.6 MB (24.23%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.java
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.java
@@ -8,12 +8,11 @@ class Solution {
 
         int e = intervals[0][1];
         int n = intervals.length;
-        int ans = n;
+        int ans = 1;
 
-        for (int i = 1; i < n; i++) {
-            if (intervals[i][1] <= e)
-                ans--;
-            else {
+        for (int i = 0; i < n; i++) {
+            if (intervals[i][1] > e) {
+                ans++;
                 e = intervals[i][1];
             }
         }
