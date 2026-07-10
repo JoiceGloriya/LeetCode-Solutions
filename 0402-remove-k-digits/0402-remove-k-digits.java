11077b2ffd193f01f255ad278810f78efe0b7727
Joice Gloriya <joicegloriyaa@gmail.com>
2026-07-10 18:41:27 +0530

    Time: 28 ms (22.71%), Space: 48.3 MB (11.88%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.java
+++ 0402-remove-k-digits/0402-remove-k-digits.java
@@ -8,34 +8,27 @@ class Solution {
             }
             stk.push(num.charAt(i));
         }
+
         StringBuilder str = new StringBuilder();
         while (!stk.isEmpty() && k > 0) { // trim-off last 'k' numbers -> the stk would be of type '123456'
             stk.pop();
             k--;
         }
-        if (stk.isEmpty())
+
+        if (stk.isEmpty())  //consider : "123" and k  = 3
             return "0";
+
         while (!stk.isEmpty()) {
             str = str.append(stk.pop());
         }
+
         str.reverse();
-        while (str.length() != 0 && str.charAt(0) == '0') // trim-off leading zeroes
+        
+        while (!str.isEmpty() && str.charAt(0) == '0') // trim-off leading zeroes
             str.deleteCharAt(0);
-        if (str.length() == 0)
+
+        if (str.isEmpty())
             return "0";
         return str.toString();
     }
-
-    // void reverse(StringBuilder str) {
-    //     int start = 0;
-    //     int end = str.length() - 1;
-    //     while (start < end) {
-    //         char temp = str.charAt(start);
-    //         str.setCharAt(start, str.charAt(end));
-    //         str.setCharAt(end, temp);
-
-    //         start++;
-    //         end--;
-    //     }
-    // }
 }
