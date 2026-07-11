fa9c0244ee6bba838a23b62286c751dd5dc459b9
Joice Gloriya <joicegloriyaa@gmail.com>
2026-07-11 22:45:38 +0530

    Time: 43 ms (93.2%), Space: 113 MB (100%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,57 +1,70 @@
 class LRUCache {
 
-    Node head = new Node(0, 0);
-    Node tail = new Node(0, 0);
-    int capacity;
-    Map<Integer, Node> mpp = new HashMap<>();
+    Node head = new Node(-1, -1);
+    Node tail = new Node(-1, -1);
+    Map<Integer, Node> cache = new HashMap<>();
+    int capacity = 0;
 
     public LRUCache(int capacity) {
         this.capacity = capacity;
+        this.cache = new HashMap<>();
         head.next = tail;
         tail.prev = head;
     }
 
     public int get(int key) {
-        if (!mpp.containsKey(key))
+        if (!cache.containsKey(key))
             return -1;
-        Node node = mpp.get(key);
+
+        Node node = cache.get(key);
         remove(node);
         addAtBeg(node);
         return node.value;
     }
 
     public void put(int key, int value) {
-        if (mpp.containsKey(key))
-            remove(mpp.get(key));
-        if (capacity == mpp.size())
+        if (cache.containsKey(key)) {
+            Node node = cache.get(key);
+            node.value = value;
+            remove(node);
+            addAtBeg(node);
+            return;
+        }
+
+        if (cache.size() == capacity) {
+            cache.remove(tail.prev.key);
             remove(tail.prev);
-        addAtBeg(new Node(key, value));
+        }
+
+        Node newNode = new Node(key, value);
+        cache.put(key, newNode);
+        addAtBeg(newNode);
     }
 
     void remove(Node node) {
-        mpp.remove(node.key);
         node.prev.next = node.next;
         node.next.prev = node.prev;
     }
 
     void addAtBeg(Node node) {
-        mpp.put(node.key, node);
         node.next = head.next;
         node.prev = head;
+        head.next.prev = node;
         head.next = node;
-        node.next.prev = node;
     }
 
-    class Node {
-        Node prev, next;
-        int key, value;
+}
 
-        public Node(int key, int value) {
-            this.key = key;
-            this.value = value;
-        }
+class Node {
+    int key, value;
+    Node prev, next;
+
+    public Node(int key, int value) {
+        this.key = key;
+        this.value = value;
+        this.prev = null;
+        this.next = null;
     }
-
 }
 
 /**
