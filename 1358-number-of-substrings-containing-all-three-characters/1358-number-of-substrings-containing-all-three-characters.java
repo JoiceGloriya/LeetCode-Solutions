3ca3d95d1a6a76c23b2faeaab7f7a8f0ca0ef1db
Joice Gloriya <joicegloriyaa@gmail.com>
2026-07-13 20:23:55 +0530

    Time: 13 ms (66.86%), Space: 46 MB (90.51%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.java
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.java
@@ -3,11 +3,10 @@ class Solution {
         // T.C : O(N) and S.C : O(1)
         int[] hash = { -1, -1, -1 };
         int ct = 0;
-        int n =s.length();
-        for (int i = n - 1; i >= 0; i--) {
+        for (int i = 0; i < s.length(); i++) {
             hash[s.charAt(i) - 'a'] = i;
             if (hash[0] != -1 && hash[1] != -1 && hash[2] != -1)
-                ct = ct +  n - Math.max(hash[0], Math.max(hash[1], hash[2])) ;
+                ct += Math.min(hash[0], Math.min(hash[1], hash[2])) + 1;
         }
         return ct;
     }
