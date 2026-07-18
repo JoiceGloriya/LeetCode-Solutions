b761699d4f89a2a6910d1fb8b13a369150afcf55
Joice Gloriya <joicegloriyaa@gmail.com>
2026-07-18 14:53:58 +0530

    Time: 47 ms (58.77%), Space: 115.8 MB (58.99%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
@@ -1,18 +1,17 @@
 class Solution {//this prob is an exact replica of 'N-MEETINGS IN A ROOM' where i was given a job to find the max num of meetings i can attend (in a non-ovrlapping manner), so return intervals.length - ans(from that prob)
+
     public int eraseOverlapIntervals(int[][] intervals) {
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[1], b[1]));
-        // int end = intervals[0][1];
-        int prev = 0;
+        int end = intervals[0][1];
         int ct = 1; //cuz first interval is always non-overlapping
         int n = intervals.length;
 
         // List<int[]> list = new ArrayList<>();
         for (int i = 1; i < n; i ++) {
             // if (list.isEmpty() || end <= intervals[0]) {
-            if (intervals[i][0] >= intervals[prev][1]) {
+            if (end <= intervals[i][0]) {
                 // list.add(intervals);
-                // end = intervals[1];
-                prev = i;
+                end = intervals[i][1]; 
                 ct++;
             }
         }
