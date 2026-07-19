cf34977f1e05fdc717bd43de3d1b765ea0326037
Joice Gloriya <joicegloriyaa@gmail.com>
2026-07-19 12:47:45 +0530

    Time: 2 ms (86.89%), Space: 44 MB (35.52%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
@@ -5,18 +5,19 @@ class Solution {
         int[] lastSeen = new int[26];
         Stack<Integer> stk = new Stack<>();
         StringBuilder str = new StringBuilder();
-        for(int i = 0; i < n; i ++) 
+        for (int i = 0; i < n; i++)
             lastSeen[s.charAt(i) - 'a'] = i;
-        for(int i = 0; i < n; i ++) {
+        for (int i = 0; i < n; i++) {
             int index = s.charAt(i) - 'a';
-            if(visited[index]) continue;
-            while(!stk.isEmpty() && stk.peek() > index && i < lastSeen[stk.peek()]) 
+            if (visited[index])
+                continue;
+            while (!stk.isEmpty() && stk.peek() > index && i < lastSeen[stk.peek()])
                 visited[stk.pop()] = false;
             stk.push(index);
             visited[index] = true;
         }
-        for(int i = 0; !stk.isEmpty() && i < n; i ++) {
-            str.append((char)(stk.pop() + 'a'));
+        for (int i = 0; !stk.isEmpty() && i < n; i++) {
+            str.append((char) (stk.pop() + 'a'));
         }
         return str.reverse().toString();
     }
