46e44749d17b2caecfaf853854510f97efcff26a
Joice Gloriya <joicegloriyaa@gmail.com>
2026-07-29 12:12:05 +0530

    Time: 2 ms (70.8%), Space: 53.3 MB (71.6%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        //Optimal(Moore's Voting Algo)
+        //TC -> O(N)  SC -> O(1)
+        int candidate = nums[0];
+        int count = 0;
+        for(int i = 0; i < nums.length; i ++) {
+            if(count == 0) {
+                count = 1;
+                candidate = nums[i];
+            }
+            else if(candidate == nums[i]) count++;
+            else count--;
+        }
+        return candidate;
+    }
+}
