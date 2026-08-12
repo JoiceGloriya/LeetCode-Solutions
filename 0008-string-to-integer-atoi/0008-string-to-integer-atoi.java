39b873f7b255a5cd227b09a4e207d29ac7da6bf0
Joice Gloriya <joicegloriyaa@gmail.com>
2026-08-12 13:24:29 +0530

    Time: 1 ms (100%), Space: 43.4 MB (97.51%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -2,29 +2,33 @@ class Solution {
     public static int myAtoi(String s) {
         if (s.isEmpty())
             return 0;
-        int result = 0, sign = 1, i = 0, n = s.length();
+        int result = 0, sign = 1, n = s.length(), i = 0;
+
         while (i < n && s.charAt(i) == ' ')
             i++;
         if (i < n && s.charAt(i) == '-') {
             sign = -1;
             i++;
-        }
-        else if (i < n && s.charAt(i) == '+') {
-            sign = 1;
+        } else if (i < n && s.charAt(i) == '+')
             i++;
-        }
         while (i < n && s.charAt(i) == '0')
             i++;
-        for (; i < n; i++) {
+        while (i < n) {
             if (Character.isDigit(s.charAt(i))) {
-                char c = s.charAt(i);
-                if ((result > Integer.MAX_VALUE / 10)
-                        || ((result == Integer.MAX_VALUE / 10) && (c - '0') > (Integer.MAX_VALUE % 10)))
-                    return (sign == -1) ? Integer.MIN_VALUE : Integer.MAX_VALUE;
-                result = result * 10 + (c - '0');
+                char c = s.charAt(i); //cuz Integer.MAX_VALUE is 2147483647
+                if (result > (Integer.MAX_VALUE / 10) || (result == (Integer.MAX_VALUE / 10)
+                        && (c - '0') > 7)) {
+                    if (sign == 1)
+                        return Integer.MAX_VALUE;
+                    else
+                        return Integer.MIN_VALUE;
+                } else
+                    result = result * 10 + (c - '0');
+                i++;
             } else
                 return result * sign;
+
         }
-        return result * sign;
+        return result * sign;  // then, u re given a valid string
     }
 }
