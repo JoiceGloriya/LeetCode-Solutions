3be48c9f96dd33807840ece5141775f369227aa4
Joice Gloriya <joicegloriyaa@gmail.com>
2026-09-09 14:08:38 +0530

    Time: 1 ms (96.65%), Space: 42.7 MB (18.03%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,6 +1,9 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if(n <= 0) return false;
+        if (n <= 0)
+            return false;
+          // A power of 2 has exactly one 1 bit.
+// For n - 1, that 1 becomes 0 and all bits after it become 1.
         return ((n & (n - 1)) == 0);
     }
 }
