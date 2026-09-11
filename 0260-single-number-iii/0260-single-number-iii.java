69dbea5257cda1b5be6ef18a9b5263b20666860e
Joice Gloriya <joicegloriyaa@gmail.com>
2026-09-11 23:14:27 +0530

    Time: 1 ms (100%), Space: 48 MB (58.9%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.java
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -1,15 +1,19 @@
 class Solution {
     public int[] singleNumber(int[] nums) {
         int xor = 0;
-        for(int i = 0; i < nums.length; i ++)
+
+        for (int i = 0; i < nums.length; i++)
             xor = (xor ^ nums[i]);
-        int rightmost = ((xor & (xor - 1)) ^ xor); //must be long, bcoz if xor is the smallest integer, then xor - 1 will overflow the capacity of integer! 
+
+        int rightmost = ((xor & (xor - 1)) ^ xor); // this will find the rightmost bit that is different in both since
+                                                   // that is easier to find..only that bit will be set and the rest will be 0
         int bucket_1 = 0, bucket_2 = 0;
-        for(int i = 0; i < nums.length; i ++) {
-            if((rightmost & nums[i]) != 0) //some non-zero number
+        for (int i = 0; i < nums.length; i++) {
+            if ((rightmost & nums[i]) != 0)
                 bucket_1 ^= nums[i];
-            else bucket_2 ^= nums[i];
+            else
+                bucket_2 ^= nums[i];
         }
-        return new int[] {bucket_1, bucket_2};
+        return new int[] { bucket_1, bucket_2 };
     }
 }
