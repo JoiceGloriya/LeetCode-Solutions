d1327d90e6d06f084ca7385676d780bff454267d
Joice Gloriya <joicegloriyaa@gmail.com>
2026-09-14 20:02:19 +0530

    Time: 33 ms (70.05%), Space: 102.4 MB (20.81%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,86 +1,47 @@
-// class Pair {
-//     int x, min;
-//     public Pair(int a, int b) {
-//         x = a;
-//         min = b;
-//     }
-// }
-// Brute: TC -> O(N) SC -> O(2N) [stores evry node in pairs]
-// class MinStack {
-
-//     Stack<Pair> stk = new Stack<>();
-
-//     public MinStack() {
-
-//     }
-
-//     public void push(int val) {
-//         int mini;
-//         if(stk.isEmpty()) mini = val;
-//         else {
-//             mini = Math.min(val, stk.peek().min);
-//         }
-//         stk.push(new Pair(val, mini));
-//     }
-
-//     public void pop() {
-//         stk.pop();
-//     }
-
-//     public int top() {
-//        return stk.peek().x; 
-//     }
-
-//     public int getMin() {
-//         return stk.peek().min;
-//     }
-// }
-
-// '2 * val - min' is used to encode the new value
-// Encoding min :-
-// When pushing a new value onto the stack, if the value is less than current minimum, we need to update the minimum. However, we also need to store the previous minimum value somewhere so that we can restore it when the minimum value is popped off in the future.
-
-// 'min = 2 * min - stk.peek()' is used to decode the previous minimum value
-// Decoding min :-
-// while poppping, if the value to be popped off is < min value, then it' s an encoded value(not a original element), so decode and return the original value![same formula]
-
 class MinStack {
-    Stack<Long> stk = new Stack<>();
-    long min = Long.MAX_VALUE;
+    Stack<Long> stk;
+    long mini;
 
     public MinStack() {
+        stk = new Stack<>();
+        mini = Long.MAX_VALUE;
     }
 
-    public void push(int val) {
-        if (stk.isEmpty()) {
-            min = val;
-            stk.push((long) val);
-        } else {
-            if (val > min) {
-                stk.push((long) val);
-            } else {
-                stk.push(2L * val - min);
-                min = val;
-            }
-        }
+    public void push(int value) {
+        long valuee = value;
+        if (valuee < mini) {
+            long modVal = (2L * valuee) - mini;
+            stk.push(modVal);
+            mini = valuee;
+        } else
+            stk.push(valuee);
     }
 
     public void pop() {
-        if (stk.peek() < min) { // this is an encoded value, so decode and return
-            min = 2 * min - stk.pop();
-        } else {
-            stk.pop();
+        long val = stk.pop(); //value == modVal
+        if (val < mini) {
+            mini = (2L * mini) - val;
         }
     }
 
     public int top() {
-        if (stk.peek() < min) {
-            return (int) min;
+        long val = stk.peek();
+        if (val < mini) {
+            return (int) mini;
         }
-        return stk.peek().intValue();
+        return (int) val;
     }
 
     public int getMin() {
-        return (int) min;
+        return (int) mini;
     }
 }
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(value);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
