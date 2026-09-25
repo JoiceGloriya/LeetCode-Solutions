8c7a5dd454f1bdc56ad6ec4273cfb29ae0cbf783
Joice Gloriya <joicegloriyaa@gmail.com>
2026-09-26 00:56:38 +0530

    Time: 23 ms (92.77%), Space: 46.5 MB (99.94%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,19 +1,27 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        Map<Long, Integer> mpp = new HashMap<>();
-        mpp.put(0L, 1);
-        long prefSum = 0;
-        int sum = 0;
-        for (int i = 0; i < nums.length; i++) {
-            prefSum += nums[i];
-            
-            long rem = prefSum - k;
-            if (mpp.containsKey(rem)) {
-                sum += mpp.get(rem);
-            }
+        //this SLINDING WINDOW approach'll not wrk since array contains -ve elemnts, so i' ll be using prefSum concept.
+        // int l = 0, r = 0, sum = 0, ct = 0;
+        // while(r < nums.length) {
+        //     sum += nums[r];
+        //     while(sum > k && (l+1) < nums.length) {
+        //         sum -= nums[l];
+        //         l++;
+        //     }
+        //     if(sum == k) ct++;
+        //     r++;
+        // }
+        // return ct; 
 
-            mpp.put(prefSum, mpp.getOrDefault(prefSum, 0) + 1);
+        Map<Integer, Integer> mpp = new HashMap<>();
+        mpp.put(0, 1);
+        int prefSum = 0, ct = 0;
+        for(int num : nums) {
+            prefSum += num;
+            if(mpp.containsKey(prefSum - k))
+                ct += mpp.get(prefSum - k);
+            mpp.put(prefSum, mpp.getOrDefault(prefSum, 0)+1);
         }
-        return sum;
+        return ct;
     }
 }
